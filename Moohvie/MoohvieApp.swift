6dbf9b3f8eacbe4c25abe3896c1b99c28201af20
Noah ONOFRE <noah.onofre.91@gmail.com>
2026-06-24 09:04:50 +0200

    Initial Commit

---
+++ Moohvie/MoohvieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoohvieApp.swift
+//  Moohvie
+//
+//  Created by Noah Onofré on 24/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoohvieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
